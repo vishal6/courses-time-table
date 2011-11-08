e5c5a60733d157a76b39e6d17b88e5f389a61dfc
mnedmea3000@gmail.com <mnedmea3000@gmail.com@2cf9ca3d-0f5d-3e33-acf8-8f3d3ddba80e>
2011-11-08 23:36:37 +0000

    V 0.1

---
+++ trunk/ courses-time-table/Course_TimeTable/src/course_timetable/procedures.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package course_timetable;
+
+/**
+ *
+ * @author Nody
+ */
+//here we will put all the procedures we will use
+public class procedures {
+    //prof
+    public String prof(){
+        String pr="select  id,first_name from prof";
+        return pr;
+    }
+    //student
+    public String std(){
+        String s="select ID,first_name from student ";
+        return s;
+    }
+    
+    //TA
+    public String ta(){
+        String t = "select id, first_name from ta";
+        return t;
+    }
+    
+    //courses
+    public  String course(){
+        String c = "select id,name from courses";
+        return c;
+    }
+    
+}
